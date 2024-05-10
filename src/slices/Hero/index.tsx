bd1da573f30c21d8e62d99c0cf3db2e87a0d6afb
Md <kmazid431@gmail.com>
2024-05-10 20:20:43 +0530

    initial step

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,29 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+        <span className="text-red-500">{slice.primary.first_name}</span>
+        <>{slice.primary.last_name}</>
+        <>{slice.primary.tag_line}</>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
